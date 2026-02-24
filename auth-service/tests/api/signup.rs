815f209d9f628ee4fc6e0d5a153302404903bd63
Ryan Kistler <rpkist77@gmail.com>
2026-02-23 22:46:23 -0600

    [Sprint 1][Task 6] feat: got json deserialization and tests working for /signup

--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,10 +1,44 @@
-use crate::helpers::TestApp;
+use crate::helpers::{get_random_email, TestApp};
 
 #[tokio::test]
-async fn signup_returns_200() {
+async fn should_return_422_if_malformed_input() {
     let app = TestApp::new().await;
 
-    let response = app.post_signup().await;
+    let random_email = get_random_email(); // Call helper method to generate email
 
-    assert_eq!(response.status().as_u16(), 200);
+    // TODO: add more malformed input test cases
+    let test_cases = [
+        serde_json::json!({
+            "password": "password123",
+            "requires2FA": true
+        }),
+        serde_json::json!({
+            "email": 321,
+            "password": "password123",
+            "requires2FA": true
+        }),
+        serde_json::json!({
+            "email": random_email,
+            "password": "password123",
+            "requires2FA": "Y"
+        }),
+        serde_json::json!({
+            "email": random_email,
+            "requires2FA": true
+        }),
+        serde_json::json!({
+            "email": random_email,
+            "password": "passwordonly",
+        }),
+    ];
+
+    for test_case in test_cases.iter() {
+        let response = app.post_signup(test_case).await; // call `post_signup`
+        assert_eq!(
+            response.status().as_u16(),
+            422,
+            "Failed for input: {:?}",
+            test_case
+        );
+    }
 }
